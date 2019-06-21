0e524f99550385c5014bc5b2e9851aa617bce6fc
Karl Peterson <kpeters4@ncsu.edu>
2019-06-21 18:36:32 -0400

    Initial commit

---
+++ RNG.py
@@ -0,0 +1,10 @@
+'''
+Created on Jun 21, 2019
+
+@author: karl
+'''
+
+import random #imports library with rng functions
+
+for x in range(0,20):           #Runs 20 times
+    print(random.randint(0,9))  #Prints a random integer between 0 and 9
